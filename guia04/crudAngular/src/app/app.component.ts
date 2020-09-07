638feeea7ffd0be6046244252554850928f513b6
waf503 <wilmerflores2012@hotmail.com>
2020-09-07 09:40:29 -0600

    Agregando los campos email, dir y telefono al crud alumnos

--- guia04/crudAngular/src/app/app.component.ts
+++ guia04/crudAngular/src/app/app.component.ts
@@ -9,12 +9,12 @@ export class AppComponent {
   title = 'crudAngular';
   //Arreglo del tipo Alumno, que tiene 3 registros almacenados
   alumnoArray: Alumno[] = [
-    {id:1, name:"Alex",lastname:"Campos",age:35},
-    {id:2, name:"Juan",lastname:"Lopez",age:20},
-    {id:3, name:"Maria",lastname:"Castro",age:23}
+    {id:1, name:"Alex",lastname:"Campos",age:35,direccion:"San Salvador",email:"alex@gmail.com",tel:72448544},
+    {id:2, name:"Juan",lastname:"Lopez",age:20,direccion:"La Union",email:"lopez@gmail.com",tel:72447734},
+    {id:3, name:"Maria",lastname:"Castro",age:23,direccion:"La Paz",email:"maria@gmail.com",tel:71668544}
   ]
   //atributo selecAlumno del tipo Alumno, por lo tanto, puede almacenar un objto
-  selectedAlumno: Alumno = {id:0, name:'', lastname:'', age:0};
+  selectedAlumno: Alumno = {id:0, name:'', lastname:'', age:0, direccion:'', email:'',tel:0};
   //un metodo que no retorna nada void, recibe como parametro una variable del
   //tipo Alumno, para ser asignada al atributo selecAlumno y poder ser mostrado
   //en pantalla.
@@ -32,7 +32,7 @@ export class AppComponent {
       this.selectedAlumno.id = this.alumnoArray.length + 1;
       this.alumnoArray.push(this.selectedAlumno);
     }
-    this.selectedAlumno = {id:0, name: '', lastname: '', age: 0};
+    this.selectedAlumno = {id:0, name: '', lastname: '', age: 0, direccion: '',email: '', tel: 0};
   }
   //Metodo que no retorna nada "void", No recibe parametro, elimina del arreglo el
   //registro, pero antes muestra en pantalla un confirmar, se recorre el arreglo
@@ -42,7 +42,7 @@ export class AppComponent {
   delete(): void{
     if(confirm('Esta seguro de eliminar el Registro?')){
       this.alumnoArray = this.alumnoArray.filter(x => x != this.selectedAlumno);
-      this.selectedAlumno = {id:0, name: '', lastname: '', age:0};
+      this.selectedAlumno = {id:0, name: '', lastname: '', age:0, direccion: '', email: '',tel: 0};
     }
   }
 }
--- guia04/crudAngular/src/app/models/alumno.ts
+++ guia04/crudAngular/src/app/models/alumno.ts
@@ -3,4 +3,7 @@ export class Alumno {
     name: string = '';
     lastname: string = '';
     age: number = 0;
+    direccion: string = '';
+    email: string = '';
+    tel: number;
 }
