78db7422cc409772a756e1446363a7cdfabd1808
waf503 <wilmerflores2012@hotmail.com>
2020-09-30 17:16:15 -0600

    Modificacion del enviroment

--- guia06/AuthAngularFirebase/src/environments/environment.prod.ts
+++ guia06/AuthAngularFirebase/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyAmQzdaJnYMGERJehx-rK4hAet0Wbza6UQ",
-    authDomain: "guia-angular-firebase.firebaseapp.com",
-    databaseURL: "https://guia-angular-firebase.firebaseio.com",
-    projectId: "guia-angular-firebase",
-    storageBucket: "guia-angular-firebase.appspot.com",
-    messagingSenderId: "252992775702",
-    appId: "1:252992775702:web:9dd8ae0511bef64c577737"
+  firebase : {
+    apiKey: "AIzaSyBtgfAWpy0klCYV6CcudehYp5hJ98UEfNU",
+    authDomain: "loginangular-f40b2.firebaseapp.com",
+    databaseURL: "https://loginangular-f40b2.firebaseio.com",
+    projectId: "loginangular-f40b2",
+    storageBucket: "loginangular-f40b2.appspot.com",
+    messagingSenderId: "917350529761",
+    appId: "1:917350529761:web:a3d9b33985af34906c55a0"
   }
 };
